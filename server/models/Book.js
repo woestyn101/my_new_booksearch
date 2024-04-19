1192ffff9e0126a4d5a568ef8dc9f28afa02e77e
Marius Du Plooy <woestyn101@gmail.com>
2024-04-19 10:06:59 -0400

    book model added

---
+++ server/models/Book.js
@@ -0,0 +1,30 @@
+const { Schema } = require('mongoose');
+
+
+const bookSchema = new Schema({
+  bookId: {
+    type: String,
+    required: true,
+  },
+  authors: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = bookSchema;
